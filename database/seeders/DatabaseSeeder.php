1b85458e0b8c553bb01c251acba9cc175dbb0256
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2023-10-22 20:09:46 +0700

    update data base seeder
    updated to create pelapor, petugas, and admin roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
+        Role::create(['name' => 'pelapor']);
+        Role::create(['name' => 'petugas']);
+        Role::create(['name' => 'admin']); 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
