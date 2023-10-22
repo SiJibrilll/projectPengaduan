4eba1f85174a8bf5dfb094b196f333168af7d3a0
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2023-10-22 20:07:05 +0700

    google controller set up

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -2,17 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
 {
     function redirect() {
-        return Socialite::driver('google')->redirect();
+        return Socialite::driver('google')->redirect(); // redirect user to SSO page
     }
 
     function callback() {
-        $user =  Socialite::driver('google')->stateless()->user();
-        dd($user);
+        $googleUser =  Socialite::driver('google')->stateless()->user(); // get account data
+        
+        try{ // lets try creating a user
+            DB::transaction(function() use($googleUser) {
+                $user = User::updateOrCreate([
+                    'google_id' => $googleUser->id, // user with this google id exist? then simply return
+                ], [
+                    'username' => $googleUser->name, // else? create a new user
+                    'email' => $googleUser->email,
+                    'google_id' => $googleUser->id,
+                ]);
+        
+                $user->assignRole('pelapor'); // give user a pelapor role
+        
+                Auth::login($user); // login the user
+            });
+
+            return redirect('/');
+        }
+        catch (\Throwable $e){
+            dd('error!! ' . $e);
+        }
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
         'password',
         'nik',
         'telepon',
-        'google_token'
+        'google_id'
     ];
 
     /**
