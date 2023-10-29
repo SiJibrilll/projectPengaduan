6acae7248906dcb2cc427b645a28d34b3fbb0d36
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2023-10-29 16:07:17 +0700

    Revert "ubah status aduan fully ready"
    This reverts commit d5a5975721493db8cf4db400ced9a4ba889d48fc.

--- resources/views/components/aduan.blade.php
+++ resources/views/components/aduan.blade.php
@@ -8,8 +8,6 @@
 
 <p> {{date("jS F, Y", strtotime($aduan->created_at))}}</p>
 
-<p> Status: {{$aduan->status}} </p>
-
 @if ('diajukan' == $aduan->status && $aduan->user_id == Auth()->user()->id)
     <a class="underline" href="/aduan/edit/{{$aduan->id}}">Ubah Aduan</a>
 @endif
@@ -23,8 +21,7 @@
 <div class="flex inline-block">
     <h1 class="mr-5"> Tanggapan </h1>
     @if((Auth()->user()->hasRole('admin') || Auth()->user()->hasRole('petugas')) && 'diajukan' !== $aduan->status)
-        {{-- <a href="/tanggapan/create"> Tambah tanggapan</a> --}}
-        
+        <a href="/tanggapan/create"> Tambah tanggapan</a>
     @endif
 </div>
 
