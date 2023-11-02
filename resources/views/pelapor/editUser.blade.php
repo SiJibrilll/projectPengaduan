a8e2c94042e3f5e44309e146d293b448828d2dc8
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2023-11-02 19:39:15 +0700

    edit users UI success

--- resources/views/pelapor/editUser.blade.php
+++ resources/views/pelapor/editUser.blade.php
@@ -1,24 +1,28 @@
 <x-pelapor>
-    <h1>Ubah data profil</h1>
+    <h1 class="mt-10 mb-6 font-bold text-[#585858] text-2xl">Ubah data profil</h1>
+
 
     <form action="/users/update/{{$user->id}}" method="POST">
         @csrf
-        <input name="username" placeholder="Masukan nama..." value="{{$user->username}}"/>
+        <h1 class="font-semibold text-[#585858] text-xl">Nama</h1>
+        <input class="w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="username" placeholder="Masukan nama..." value="{{$user->username}}"/>
+        
+        <h1 class="font-semibold text-[#585858] text-xl mt-4">NIK</h1>
+        <input class="w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="nik" placeholder="Masukan NIK" value="{{$user->nik}}"/>
+        
+        <h1 class="font-semibold text-[#585858] text-xl mt-4">Telepon</h1>
+        <input class="w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="telepon" placeholder="Masukan telepon" value="{{$user->telepon}}"/>
 
-        <input name="nik" placeholder="Masukan NIK" value="{{$user->nik}}"/>
-
-        <input name="telepon" placeholder="Masukan telepon" value="{{$user->telepon}}"/>
-
-        <button type="submit" class="btn btn-primary">Simpan</button>
+        <div class="flex mt-10">
+            <button onclick="goBack()" type="button" class="ml-36 font-bold rounded-full flex-2 bg-[#E47272] text-white py-2 px-9 sm:px-5 mr-2">Batal</button>
+            <button type="submit" class="font-bold rounded-full flex-1 bg-[#31CAB8] text-white py-2 px-4 sm:px-6 ml-2">Simpan</button>
+        </div>
     </form>
 
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+<script>
+    function goBack() {
+        window.location.href = '/users/show/' + @json($user->id);
+    }
+</script>
+
 </x-pelapor>
