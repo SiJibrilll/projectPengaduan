a657867c08d1d589a94eaa0af7ad1d9dd6877716
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2023-11-03 16:36:39 +0700

    add hover effect on buttons in page edit password and edit profil pelapor

--- resources/views/pelapor/editPassword.blade.php
+++ resources/views/pelapor/editPassword.blade.php
@@ -7,8 +7,8 @@
             <input class=" mt-6 w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="new_password" type="password" placeholder="Masukan password baru..." required>
             <input class=" mt-6 w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="new_password_confirmation" type="password" placeholder="Ulangi password baru..." required>
             <div class="flex mt-10">
-                <button onclick="goBack()" type="button" class="ml-36 font-bold rounded-full flex-2 bg-[#E47272] text-white py-2 px-9 sm:px-5 mr-2">Batal</button>
-                <button type="submit" class="font-bold rounded-full flex-1 bg-[#31CAB8] text-white py-2 px-4 sm:px-6 ml-2">Simpan</button>
+                <button onclick="goBack()" type="button" class="ml-36 font-bold rounded-full flex-2 bg-[#E47272] transition-all hover:bg-[#d16868] text-white py-2 px-9 sm:px-5 mr-2">Batal</button>
+                <button type="submit" class="font-bold rounded-full flex-1 bg-[#31CAB8]  hover:bg-[#2fbfae] transition-all text-white py-2 px-4 sm:px-6 ml-2">Simpan</button>
             </div>
         </form>
         
