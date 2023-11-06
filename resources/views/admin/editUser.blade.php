a17d427547cca20e0b77b025d8ede1daceac98ed
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2023-11-06 11:43:58 +0700

    admin edit pelapor and petugas ui success

--- resources/views/admin/editUser.blade.php
+++ resources/views/admin/editUser.blade.php
@@ -1,23 +1,37 @@
 <x-admin>
-    <h1>Ubah data {{$user->getRoleNames()->first()}} </h1>
+    <h1  class="mt-5 font-bold text-[#585858] text-3xl mb-20">Ubah data {{'pelapor' == $user->getRoleNames()->first()? 'Masyarakat' : 'Petugas'}} </h1>
 
-    <form action="/users/update/{{$user->id}}" method="POST">
+    <form class="max-w-sm" action="/users/update/{{$user->id}}" method="POST">
         @csrf
-        <input name="username" placeholder="Masukan nama..." value="{{$user->username}}"/>
+        <h1 class="font-semibold mt-5 text-[#585858] text-xl">Nama</h1>
+        <input class="w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="username" placeholder="Masukan nama..." value="{{$user->username}}"/>
 
         @if ($user->hasRole('pelapor'))
-
-        <input name="nik" placeholder="Masukan NIK" value="{{$user->nik}}"/>
+        <h1 class="font-semibold mt-5 text-[#585858] text-xl">NIK</h1>
+        <input class="w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="nik" placeholder="Masukan NIK" value="{{$user->nik}}"/>
         
         @elseif ($user->hasRole('petugas'))
         
-        <input name="email" placeholder="Masukan email..." value="{{$user->email}}"/>
-
+        <h1 class="font-semibold mt-5 text-[#585858] text-xl">Email</h1>
+        <input class="w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="email" placeholder="Masukan email..." value="{{$user->email}}"/>
+        
         @endif
+        
+        <h1 class="font-semibold mt-5 text-[#585858] text-xl">Telepon</h1>
+        <input class="w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="telepon" placeholder="Masukan telepon" value="{{$user->telepon}}"/>
+        <h1 class="font-semibold mt-5 text-[#585858] text-xl">Password</h1>
+        <input class="w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="password" placeholder="Masukan password"/>
 
-        <input name="telepon" placeholder="Masukan telepon" value="{{$user->telepon}}"/>
-        <input name="password" placeholder="Masukan password"/>
-
-        <button type="submit" class="btn btn-primary">Simpan</button>
+        <div class="flex mt-10 justify-start sm:justify-end">
+            <button onclick="goBack()" type="button" class="font-bold rounded-full bg-[#E47272] transition-all hover:bg-[#d16868] text-white py-2 px-5 mr-2">Batal</button>
+            <button type="submit" class="font-bold rounded-full bg-[#31CAB8] min-w-max hover:bg-[#2fbfae] transition-all text-white py-2 px-8 ml-2">Simpan</button>
+        </div>
     </form>
+
+    <script>
+        function goBack() {
+            window.location.href = '/users/show/' + @json($user->id);
+        }
+    </script>
+    
 </x-admin>
