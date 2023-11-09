642df75da338fbb9a8e430ccad50f3fbd0cc22fc
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2023-11-09 18:06:14 +0700

    added error ui to ubah password

--- resources/views/pelapor/editPassword.blade.php
+++ resources/views/pelapor/editPassword.blade.php
@@ -19,6 +19,9 @@
         @csrf
         <input class="w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="new_password" type="password" placeholder="Masukan password baru..." required>
         <input class=" mt-6 w-full border-b-2 border-[#ABABAB] focus:border-gray-600" name="new_password_confirmation" type="password" placeholder="Ulangi password baru..." required>
+        @error('new_password')
+            <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
+        @enderror
         <div class="flex mt-10 justify-end">
             <button onclick="goBack()" type="button" class="font-bold rounded-full bg-[#E47272] transition-all hover:bg-[#d16868] text-white py-2 px-5 mr-2">Batal</button>
             <button type="submit" class="font-bold rounded-full bg-[#31CAB8] min-w-max hover:bg-[#2fbfae] transition-all text-white py-2 px-8 ml-2">Simpan</button>
