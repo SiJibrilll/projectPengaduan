8393f1aa2afc7a241d6ce3c667496657ae70df91
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2023-11-09 15:55:00 +0700

    Revert "Revert "oopsie""
    This reverts commit 1ea93163e5fbdf5a503b33260808492704432267.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,5 +33,12 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $petugas->assignRole('petugas');
+
+        $pelapor = \App\Models\User::factory()->create([
+            'username' => 'petugas',
+            'email' => 'petugas@LaporIND.com',
+            'password' => 12345678,
+            'telepon' => '0812 8822 3567 9167'
+        ]);
     }
 }
