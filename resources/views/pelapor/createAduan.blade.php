9a0b92fceb5a3d564257632772d1ce2c857e6223
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2023-11-09 16:23:32 +0700

    added error ui in create aduan

--- resources/views/pelapor/createAduan.blade.php
+++ resources/views/pelapor/createAduan.blade.php
@@ -2,18 +2,28 @@
 <form class="mt-24" action="/aduan/store" method="POST" enctype="multipart/form-data">
     @csrf
     <h1 class="font-semibold text-[#585858] text-2xl">Judul</h1>
-    <input class="mt-4 w-full border-b-2 border-[#ABABAB] focus:border-gray-600" type="text" name="judul" id="" placeholder="Masukan inti aduan disini">
+    <input class="mt-4 w-full border-b-2 border-[#ABABAB] focus:border-gray-600" type="text" name="judul" id="" value="{{old('judul')}}" placeholder="Masukan inti aduan disini">
+    @error('judul')
+        <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
+    @enderror
     <h1 class="font-semibold text-[#585858] text-2xl mt-8">Deskripsi</h1>
     <div class="bg-white rounded-lg shadow-md p-4">
-        <textarea name="deskripsi" placeholder="Deskripsikan aduan dengan lebih detail..." class="w-full h-32 border border-gray-300 rounded p-2 resize-none" placeholder="Enter your text..."></textarea>
+        <textarea name="deskripsi" placeholder="Deskripsikan aduan dengan lebih detail..." class="w-full h-32 border border-gray-300 rounded p-2 resize-none" placeholder="Enter your text...">{{old('deskripsi')}} </textarea>
     </div>
+    @error('deskripsi')
+        <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
+    @enderror
       
     <h1 class="font-semibold text-[#585858] text-2xl mt-8 mb-4">Gambar</h1>
     <input class="filepond" type="file" name='image[]' multiple credits='false'>
+    @error('image')
+        <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
+    @enderror
     <div class="flex mt-24">
         <button onclick="goBack()" type="button" class="font-bold rounded-full flex-2 bg-[#E47272] transition-all hover:bg-[#d16868] text-white py-2 px-9 sm:px-5 mr-2">Batal</button>
         <button type="submit" class="font-bold rounded-full flex-1 bg-[#31CAB8] hover:bg-[#2fbfae] transition-all text-white py-2 px-4 sm:px-6 ml-2">Upload Aduan</button>
     </div>
+    
       
       
       
